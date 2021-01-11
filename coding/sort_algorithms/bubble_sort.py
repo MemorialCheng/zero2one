7ce226acaec54742e082e8772bc8ea9a48650d64
MemorialCheng <chengzhou115@163.com>
2021-01-11 18:08:53 +0800

    新增冒泡排序

--- coding/sort_algorithms/bubble_sort.py
+++ coding/sort_algorithms/bubble_sort.py
@@ -1,1 +1,26 @@
-# 冒泡排序
+
+
+def bubble_sort(list):
+    """
+    冒泡排序
+    :param list:
+    :return:
+    """
+    n = len(list)
+    for i in range(0, n-1):
+        is_sorted = True
+        for j in range(0, n - i - 1):
+            if list[j] > list[j + 1]:
+                list[j], list[j + 1] = list[j + 1], list[j]
+                is_sorted = False
+        if is_sorted:
+            return
+
+
+if __name__ == '__main__':
+    print("=====排序前=========")
+    test_list = [3, 6, 1, 8, 5, -20, 100, 50, 200, -32, 123]
+    print(test_list)
+    print("=====排序后=========")
+    bubble_sort(test_list)
+    print(test_list)
