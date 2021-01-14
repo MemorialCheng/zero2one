3402d794d6e63ff7bf49b8e5535111026582f128
MemorialCheng <chengzhou115@163.com>
2021-01-14 12:11:34 +0800

    更新冒泡排序

--- Coding/sort_algorithms/bubble_sort.py
+++ Coding/sort_algorithms/bubble_sort.py
@@ -4,7 +4,12 @@
 @File ：selection_sort.py
 @Author ：cheng
 @Date ：2021/1/11
-@Description : 冒泡排序
+@Description :
+    冒泡排序：比较相邻两个数字大小，将比较大（比较小）的那个数交换到后面，不断的交换下去就可以将最大（最小）的那个数放在队列的尾部，
+            然后重头再次交换，直到将数列排成有序数列。
+    说明：从后往前改变（最大/最小放后面），换言之，先锁定尾部为有序数列。
+    复杂度： 最优时间复杂度 O(n), 最坏时间复杂度 O(n^2)
+    稳定性： 稳定
 """
 
 
@@ -15,7 +20,9 @@ def bubble_sort(list):
     :return:
     """
     n = len(list)
-    for i in range(0, n-1):
+    if n <= 1:
+        return list
+    for i in range(0, n - 1):
         is_sorted = True
         for j in range(0, n - i - 1):
             if list[j] > list[j + 1]:
