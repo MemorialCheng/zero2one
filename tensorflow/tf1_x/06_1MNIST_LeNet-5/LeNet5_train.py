943eea344f5f8fd09a855386d2548e70e658226c
MemorialCheng <chengzhou115@163.com>
2021-01-18 09:48:16 +0800

    MNIST手写数字识别_LeNet-5实现

--- tensorflow/tf1_x/06_1MNIST_LeNet-5/LeNet5_train.py
+++ tensorflow/tf1_x/06_1MNIST_LeNet-5/LeNet5_train.py
@@ -21,6 +21,8 @@ LEARNING_RATE_DECAY = 0.99
 REGULARIZATION_RATE = 0.0001
 TRAINING_STEPS = 6000
 MOVING_AVERAGE_DECAY = 0.99
+MODEL_SAVE_PATH = "MNIST_model/"
+MODEL_NAME = "mnist_model"
 
 
 # 2.定义训练过程
@@ -36,6 +38,7 @@ def train(mnist):
 
     regularizer = tf.contrib.layers.l2_regularizer(REGULARIZATION_RATE)
     y = LeNet5_inference.inference(x, False, regularizer)
+
     global_step = tf.Variable(0, trainable=False)
 
     # 定义损失函数、学习率、滑动平均操作以及训练过程。
@@ -47,10 +50,12 @@ def train(mnist):
     learning_rate = tf.train.exponential_decay(
         LEARNING_RATE_BASE,
         global_step,
-        mnist.train.num_examples / BATCH_SIZE, LEARNING_RATE_DECAY,
+        mnist.train.num_examples / BATCH_SIZE,
+        LEARNING_RATE_DECAY,
         staircase=True)
 
     train_step = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss, global_step=global_step)
+
     with tf.control_dependencies([train_step, variables_averages_op]):
         train_op = tf.no_op(name='train')
 
@@ -58,6 +63,7 @@ def train(mnist):
     saver = tf.train.Saver()
     with tf.Session() as sess:
         tf.global_variables_initializer().run()
+
         for i in range(TRAINING_STEPS):
             xs, ys = mnist.train.next_batch(BATCH_SIZE)
 
@@ -70,6 +76,7 @@ def train(mnist):
 
             if i % 1000 == 0:
                 print("After %d training step(s), loss on training batch is %g." % (step, loss_value))
+                saver.save(sess, os.path.join(MODEL_SAVE_PATH, MODEL_NAME), global_step=global_step)
 
 
 # 3. 主程序入口
