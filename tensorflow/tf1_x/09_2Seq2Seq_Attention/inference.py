cf8ae5c16a7b795eb47bd7205ebc50386c062a42
MemorialCheng <chengzhou115@163.com>
2021-02-03 17:57:48 +0800

    seq2seq_attention项目：Decoder

--- tensorflow/tf1_x/09_2Seq2Seq_Attention/inference.py
+++ tensorflow/tf1_x/09_2Seq2Seq_Attention/inference.py
@@ -7,8 +7,6 @@
 @Description : 
 """
 import tensorflow as tf
-import codecs
-import sys
 import json
 
 # 读取checkpoint的路径。4800表示是训练程序在第4800步保存的checkpoint
@@ -17,7 +15,7 @@ CHECKPOINT_PATH = 'Trained_model/seq2seq_ckpt-1400'
 # 模型参数。必须与训练时的模型参数保持一致
 HIDDEN_SIZE = 1024  # LSTM的隐藏层规模
 NUM_LAYERS = 2  # 深层循环神经网络中LSTM结构的层数
-SRC_VOCAB_SIZE = 400000  # 源语言词汇表大小
+SRC_VOCAB_SIZE = 40000  # 源语言词汇表大小
 TRG_VOCAB_SIZE = 4000  # 目标语言词汇表大小
 SHARE_EMB_AND_SOFTMAX = True  # 在softmax层和词向量层之间共享参数
 
@@ -159,12 +157,6 @@ def main():
 
     # 根据英文词汇表，将测试句子转为单词ID。
     test_en_ids = convert_input("en", test_en_text)
-    # with codecs.open(SRC_VOCAB, "r", "utf-8") as vocab:
-    #     src_vocab = [w.strip() for w in vocab.readlines()]
-    #     # 运用dict,　将单词和id对应起来组成字典，用于后面的转换。
-    #     src_id_dict = dict((src_vocab[x], x) for x in range(SRC_VOCAB_SIZE))
-    # test_en_ids = [(src_id_dict[en_text] if en_text in src_id_dict else src_id_dict['<unk>'])
-    #                for en_text in test_en_text.split()]
     print(test_en_ids)
 
     # 建立解码所需的计算图。
@@ -179,9 +171,6 @@ def main():
 
     # 根据中文词汇表，将翻译结果转换为中文文字。
     output_text = convert_input("zh", output_ids)
-    # with codecs.open(TRG_VOCAB, "r", "utf-8") as f_vocab:
-    #     trg_vocab = [w.strip() for w in f_vocab.readlines()]
-    # output_text = ''.join([trg_vocab[x] for x in output_ids])
 
     # 输出翻译结果。 utf-8编码
     # print(output_text.encode('utf8'))
